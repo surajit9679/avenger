843f428bf8da9ba4ff49a9a3eefa638de09e0e45
surajit9679 <87864130+surajit9679@users.noreply.github.com>
2021-07-24 10:25:23 +0530

    Add files via upload

---
+++ controller/BaseController.php
@@ -0,0 +1,59 @@
+<?php
+namespace App\Controllers;
+
+/**
+ * Class BaseController
+ *
+ * BaseController provides a convenient place for loading components
+ * and performing functions that are needed by all your controllers.
+ * Extend this class in any new controllers:
+ *     class Home extends BaseController
+ *
+ * For security be sure to declare any new methods as protected or private.
+ *
+ * @package CodeIgniter
+ */
+
+use CodeIgniter\Controller;
+
+class BaseController extends Controller
+{
+
+	/**
+	 * An array of helpers to be loaded automatically upon
+	 * class instantiation. These helpers will be available
+	 * to all other controllers that extend BaseController.
+	 *
+	 * @var array
+	 */
+	protected $helpers = [];
+
+	/**
+	 * Constructor.
+	 */
+	public function initController(\CodeIgniter\HTTP\RequestInterface $request, \CodeIgniter\HTTP\ResponseInterface $response, \Psr\Log\LoggerInterface $logger)
+	{
+		// Do Not Edit This Line
+		parent::initController($request, $response, $logger);
+
+		//--------------------------------------------------------------------
+		// Preload any models, libraries, etc, here.
+		//--------------------------------------------------------------------
+		// E.g.:
+		// $this->session = \Config\Services::session();
+	}
+
+	public function primari($page){
+			echo view('includes/pagesource');
+			echo view($page);
+	}
+
+	public function page($page,$data =null){
+		echo view('includes/pagesource',$data);
+		echo view('includes/header',$data);
+		echo view('includes/left-menu',$data);
+		echo view($page,$data);
+		echo view('includes/footer',$data);
+	}
+
+}
